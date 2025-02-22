97531939ade38d00e358bc35f46da62f2351d398
Biglens <adedejibioku@gmail.com>
2025-02-22 21:09:50 +0100

    deployed

--- integration_with_telex/src/main.ts
+++ integration_with_telex/src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe()); // Optional, helps with request validation
+  app.useGlobalPipes(new ValidationPipe()); 
+  const port = process.env.PORT || 3000
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
 
