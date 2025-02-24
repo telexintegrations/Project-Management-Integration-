04e8d296e5062bec2e241564ada6679aa97a9479
Biglens <adedejibioku@gmail.com>
2025-02-24 13:14:44 +0100

    checking for updated version of .env file for render deployment

--- integration_with_telex/src/main.ts
+++ integration_with_telex/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv'
-
-dotenv.config();
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe()); 
-  const port = process.env.PORT || 3000
-
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule, { logger: ['log', 'warn', 'error', 'debug', 'verbose'] });
+  await app.listen(3000);
 }
 bootstrap();
+
 
 
